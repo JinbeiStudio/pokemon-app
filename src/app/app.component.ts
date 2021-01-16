dfcc763a494b481871f1b64c21098da3740212dd
EDurdos <emeric.durdos@gmail.com>
2021-01-16 18:12:42 +0000

    remplissage des faux pokemon pour test css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,99 @@ export class AppComponent {
           attaque: 65,
           vitesse: 45
         }
+      },
+      {
+        name: "Fearow",
+        element: "Normal",
+        img:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/22.png",
+        stats: {
+          pv: 100,
+          defense: 61,
+          attaque: 100,
+          vitesse: 100
+        }
+      },
+      {
+        name: "bulbasaur",
+        element: "Grass",
+        img:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        stats: {
+          pv: 45,
+          defense: 65,
+          attaque: 65,
+          vitesse: 45
+        }
+      },{
+        name: "Fearow",
+        element: "Normal",
+        img:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/22.png",
+        stats: {
+          pv: 100,
+          defense: 61,
+          attaque: 100,
+          vitesse: 100
+        }
+      },
+      {
+        name: "bulbasaur",
+        element: "Grass",
+        img:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        stats: {
+          pv: 45,
+          defense: 65,
+          attaque: 65,
+          vitesse: 45
+        }
+      },{
+        name: "Fearow",
+        element: "Normal",
+        img:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/22.png",
+        stats: {
+          pv: 100,
+          defense: 61,
+          attaque: 100,
+          vitesse: 100
+        }
+      },
+      {
+        name: "bulbasaur",
+        element: "Grass",
+        img:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        stats: {
+          pv: 45,
+          defense: 65,
+          attaque: 65,
+          vitesse: 45
+        }
+      },{
+        name: "Fearow",
+        element: "Normal",
+        img:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/22.png",
+        stats: {
+          pv: 100,
+          defense: 61,
+          attaque: 100,
+          vitesse: 100
+        }
+      },
+      {
+        name: "bulbasaur",
+        element: "Grass",
+        img:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        stats: {
+          pv: 45,
+          defense: 65,
+          attaque: 65,
+          vitesse: 45
+        }
       }
-  
       ];
 }
